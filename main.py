6fcfa8bd4b965d5b08bfb99bfbaf6a42612474c8
AdrianoGC24 <adrianogonzalezcera002@gmail.com>
2026-03-05 08:27:31 -0500

    feat: agegué la condición de que no pueden haber notas mayores a 5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+"""
 try:
     print("==============================")
     print("=====Tabla de multiplicar=====")
@@ -12,3 +12,73 @@ try:
         
 except ValueError:
     print("solo puedes colocar numeros")
+"""
+    ##################################################
+import time
+
+print("""      ==========================================
+      =======REGISTRO DE ESTUDIANTES RIWI=======
+      ==========================================""")
+
+try:
+
+
+    cantidad_estudiantes= int(input("¿CUANTOS ESTUDIANTES HAY?: "))
+    promedio_general = 0
+    estudiantes_aprobados = 0
+    estudiantes_reprobados = 0
+    
+    for estudiantes in (1, cantidad_estudiantes+1):
+
+        estudiante = str(input("\nINGRESE SU NOMBRE: "))
+
+        nota1 = float(input("INGRESE LA PRIMERA NOTA: "))
+        nota2 = float(input("INGRESE LA SEGUNDA NOTA: "))
+        nota3 = float(input("INGRESE LA TERCERA NOTA: "))
+
+        while True:
+            if nota1>5 or nota2>5 or nota3 >5:
+                print("\n¡No se admiten notas mayores a 5!")
+                nota1 = float(input("INGRESE LA PRIMERA NOTA: "))
+                nota2 = float(input("INGRESE LA SEGUNDA NOTA: "))
+                nota3 = float(input("INGRESE LA TERCERA NOTA: "))
+                
+            else:
+                False
+                break
+            
+
+        promedio_estudiante = round((nota1+nota2+nota3)/3 ,1)
+        
+
+        if promedio_estudiante>=3:
+            print("Calculando promedio...")
+            time.sleep(1)
+            print(f"El estudiante {estudiante} tiene un promedio de {promedio_estudiante} - ¡Has aprobado!")
+            estudiantes_aprobados += 1
+            promedio_general += promedio_estudiante
+            time.sleep(2)
+            
+            
+
+        elif promedio_estudiante<3:
+            print("Calculando promedio...")
+            time.sleep(1)
+            print(f"El estudiante {estudiante} tiene un promedio de {promedio_estudiante} - ¡Has reprobado!")
+            estudiantes_reprobados += 1
+            promedio_general += promedio_estudiante
+            time.sleep(2)
+    
+    print(f"\nHay un total de {cantidad_estudiantes} estudiantes.")
+    print(f"{estudiantes_aprobados} estudiantes aprobados y {estudiantes_reprobados} estudiantes reprobados")
+    print(f"con un promedio general de {promedio_general/cantidad_estudiantes}")
+    time.sleep(2)
+
+
+
+except ValueError:
+    print("Coloque el valor pedido, solo numeros o solo letras")
+
+
+
+
